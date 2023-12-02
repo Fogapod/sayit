f18d85ba806f260debd6622028adb078e23c044e
Eugene <fogaprod@gmail.com>
2023-12-02 18:59:26 +0400

    python line iterator is 10 times faster than shell and also filters garbage

--- scripts/flamegraph.sh
+++ scripts/flamegraph.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/sh
 
 echo building CLI with debug enabled
-CARGO_PROFILE_RELEASE_DEBUG=true cargo build --release --features cli
+CARGO_PROFILE_RELEASE_DEBUG=true CARGO_PROFILE_RELEASE_STRIP=false cargo build --release --features cli
 
-echo recording perfomance for 10 seconds
+echo recording perfomance for 20 seconds
 # infinitely cycle through sample_text.txt and send lines to cli
-timeout 10 sh -c 'while true; do cat tests/sample_text.txt; done | while read line; do echo "$line"; done' | CARGO_PROFILE_RELEASE_DEBUG=true perf record --call-graph dwarf -- target/release/pink_accents -a examples/linux.ron > /dev/null
+timeout 20 python -c 'for l in __import__("itertools").cycle(l.strip() for l in open("tests/sample_text.txt").readlines() if l.strip() and l != " :\n"): print(l)' | pv | perf record --call-graph dwarf -- target/release/pink_accents -a examples/linux.ron > /dev/null
 
 echo folding stacks
 perf script | inferno-collapse-perf > stacks.folded
